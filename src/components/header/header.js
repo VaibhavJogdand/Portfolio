aa0caa42e3a96a4c12a698f7f4f6de04e185d336
vaibhav_jogdand <vrjogdand708@gmail.com>
2021-08-31 12:00:03 +0530

    Added link to header button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,8 +8,7 @@ export default function Header(){
             <div className='header-intro'>
                 <h4 className='intro-name'> I'm <span className='name-design'>Vaibhav Jogdand</span></h4>
                 <h2>WEB DEVELOPER</h2>
-                <button className='header-btn btn btn-outline-warning'>CONTACT ME</button>
-                
+                <a href='#contact'><button className='header-btn btn btn-outline-warning'>CONTACT ME</button></a>
             </div>
             <div className='header-img'>
                 <img className='headerSvg' src='https://www.maavan.com/images/banner%20image.svg'></img>
